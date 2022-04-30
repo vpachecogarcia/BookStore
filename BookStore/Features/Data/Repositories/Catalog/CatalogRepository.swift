8f0f1401e05b29081a2220b334470b6edd37097c
victor.pacheco <victor.pacheco@soptrasteria.es>
2022-04-30 18:23:55 +0200

    WIP: Added mocks to handle pagination

--- BookStore/Features/Data/Repositories/Catalog/CatalogRepository.swift
+++ BookStore/Features/Data/Repositories/Catalog/CatalogRepository.swift
@@ -14,17 +14,31 @@ final class DefaultCatalogRepository: CatalogRepository {
     }
     
     func getCatalog(parameters: CatalogRepositoryParameters, completion: @escaping (Result<[BookBasicInfoDecodable], DataTransferError>) -> Void) {
-        let json = Mocks.catalogSuccess.data
-        let decoder = JSONDecoder()
         
-        do {
-            let catalogDecodable = try decoder.decode(BookListDecodable.self, from: json)
-            completion(.success(catalogDecodable))
-        } catch {
-            let error = DataTransferError.service(code: 400, title: "BAD REQUEST", detail: nil)
-            completion(.failure(error))
+        let json = getJson(for: parameters)
+        let decoder = JSONDecoder()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            do {
+                let catalogDecodable = try decoder.decode(BookListDecodable.self, from: json)
+                completion(.success(catalogDecodable))
+            } catch {
+                let error = DataTransferError.service(code: 400, title: "BAD REQUEST", detail: nil)
+                completion(.failure(error))
+            }
         }
     }
     
+    private func getJson(for params: CatalogRepositoryParameters) -> Data {
+        guard let offset = params.offset else {
+            return Mocks.catalogPage1Success.data
+        }
+        if offset < 10 {
+            return Mocks.catalogPage1Success.data
+        } else if offset < 20 {
+            return Mocks.catalogPage2Success.data
+        } else {
+            return Mocks.catalogEmptyData.data
+        }
+    }
     
 }
--- BookStore/Features/Mocks/Mocks.swift
+++ BookStore/Features/Mocks/Mocks.swift
@@ -9,14 +9,17 @@ import Foundation
 
 enum Mocks {
     
-    case catalogSuccess
+    case catalogPage1Success
+    case catalogPage2Success
     case catalogEmptyData
     case badRequest
     
     var data: Data {
         switch self {
-        case .catalogSuccess:
-            return Data(Constants.catalogSuccessMock.utf8)
+        case .catalogPage1Success:
+            return Data(Constants.catalogPage1Success.utf8)
+        case .catalogPage2Success:
+            return Data(Constants.catalogPage2Success.utf8)
         case .catalogEmptyData:
             return Data(Constants.catalogEmptyDataMock.utf8)
         case .badRequest:
@@ -25,7 +28,7 @@ enum Mocks {
     }
     
     enum Constants {
-        static let catalogSuccessMock = """
+        static let catalogPage1Success = """
             [{
                 "id": 1,
                 "link": "/api/v1/items/1",
@@ -75,6 +78,49 @@ enum Mocks {
                 "id": 10,
                 "link": "/api/v1/items/10",
                 "title": "Lolita"
+            }]
+            """
+        static let catalogPage2Success = """
+            [{
+                "id": 11,
+                "link": "/api/v1/items/11",
+                "title": "The Odyssey"
+            },{
+                "id": 12,
+                "link": "/api/v1/items/12",
+                "title": "Madame Bovary"
+            },{
+                "id": 13,
+                "link": "/api/v1/items/13",
+                "title": "The Divine Comedy"
+            },{
+                "id": 14,
+                "link": "/api/v1/items/14",
+                "title": "Alice's Adventures in Wonderland"
+            },{
+                "id": 15,
+                "link": "/api/v1/items/15",
+                "title": "To the Lighthouse"
+            },{
+                "id": 16,
+                "link": "/api/v1/items/16",
+                "title": "The Sound and the Fury"
+            },{
+                "id": 17,
+                "link": "/api/v1/items/17",
+                "title": "The Grapes of Wrath"
+            },{
+                "id": 18,
+                "link": "/api/v1/items/18",
+                "title": "Invisible Man"
+            },{
+                "id": 19,
+                "link": "/api/v1/items/19",
+                "title": "Gulliver's Travels"
+            },{
+                "id": 20,
+                "link": "/api/v1/items/20",
+                "title": "The Stranger"
             }]
             """
         static let catalogEmptyDataMock = "{[]}"
