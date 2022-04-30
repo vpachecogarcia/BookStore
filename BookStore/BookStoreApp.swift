fbc8cd9ac5a913c272036c949d8ed6656668d541
victor.pacheco <victor.pacheco@soptrasteria.es>
2022-04-30 10:48:21 +0200

    Initial Commit

---
+++ BookStore/BookStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookStoreApp.swift
+//  BookStore
+//
+//  Created by sopra on 30/4/22.
+//
+
+import SwiftUI
+
+@main
+struct BookStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
