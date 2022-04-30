ec14f4112f27b87f51b0f8e584a50668931acdc2
victor.pacheco <victor.pacheco@soptrasteria.es>
2022-04-30 20:54:48 +0200

    WIP: Added empty state and error views

---
+++ BookStore/Features/Presentation/ErrorView/ErrorView.swift
@@ -0,0 +1,20 @@
+//
+//  ErrorView.swift
+//  BookStore
+//
+//  Created by Victor Pacheco on 30/4/22.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ErrorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ErrorView()
+    }
+}
