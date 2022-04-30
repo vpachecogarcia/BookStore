2e6a4168c89366991cf4fb192a63ca4da4b284b3
victor.pacheco <victor.pacheco@soptrasteria.es>
2022-04-30 20:56:26 +0200

    WIP: Added Spinner

---
+++ BookStore/Features/Presentation/SpinnerView/SpinnerView.swift
@@ -0,0 +1,20 @@
+//
+//  SpinnerView.swift
+//  BookStore
+//
+//  Created by sopra on 30/4/22.
+//
+
+import SwiftUI
+
+struct SpinnerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SpinnerView_Previews: PreviewProvider {
+    static var previews: some View {
+        SpinnerView()
+    }
+}
