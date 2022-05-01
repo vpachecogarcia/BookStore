088845ece59e3a400f2cb23f759c2fc5f6c4f12f
victor.pacheco <victor.pacheco@soptrasteria.es>
2022-05-01 20:22:34 +0200

    WIP: Add navigation

--- BookStore/Features/Presentation/Catalog/View/CatalogRowView.swift
+++ BookStore/Features/Presentation/Catalog/View/CatalogRowView.swift
@@ -12,11 +12,15 @@ struct CatalogRowView: View {
     var book: BookBasicInfoModel
     
     var body: some View {
-        Text(book.title)
-            .lineLimit(2)
-            .foregroundColor(.white)
-            .listRowBackground(Color.stormCloud)
-            .listRowSeparatorTint(Color.weldonBlue)
-            .padding(.vertical, 16)
+        NavigationLink {
+            BookDetailView(viewModel: DefaultBookDetailViewModel(link: book.link))
+        } label: {
+            Text(book.title)
+                .lineLimit(2)
+                .foregroundColor(.white)
+                .padding(.vertical, 16)
+        }
+        .listRowBackground(Color.stormCloud)
+        .listRowSeparatorTint(Color.weldonBlue)
     }
 }
